c293aedeaebc4f014039349aa7245dc5fdf32a23
Syed Abdul Khader S S <sssyed@gmail.com>
2024-08-31 14:58:38 +0530

    changes

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -2,17 +2,37 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: '',
+      fullname: ''
     };
   },
-  methods: {
-    outputFullname() {
-      console.log('Running again...')
-      if (this.name === '') {
-        return '';
+  watch: {
+    name(value) {
+      if (value === '') {
+        this.fullname = '';
+      } else {
+        this.fullname = value + ' ' + 'Abdul Khader'
       }
-      return this.name + ' ' + 'Abdul Khader';
     },
+    lastName(value) {
+      if (value === '') {
+        this.fullname = '';
+      } else {
+        this.fullname = value + ' ' + 'Abdul Khader'
+      }
+    }
+  },
+  computed: {
+    // fullname() {
+    //   console.log('Running again...')
+    //   if (this.name === '') {
+    //     return '';
+    //   }
+    //   return this.name + ' ' + 'Abdul Khader';
+    // }
+  },
+  methods: {
     setName(event) {
       this.name = event.target.value;
     },
